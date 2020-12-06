34439ba85a744f675e7940f7ed6266304acc831e
togacoder <bike.dio@gmail.com>
2020-12-06 23:12:17 +0900

    dockerの準備

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 TAG=$1;
+if [ -z "$TAG" ]; then
+    TAG="app"
+fi
 export tag=$TAG
 docker build -t $TAG app/.
 IMAGE=`docker images | perl -wnale '/$ENV{tag}/ and print $F[2]'`
-docker run --rm -it $IMAGE /bin/bash
+docker run -u admin --rm -it $IMAGE /bin/bash
 
 #export image=$IMAGE
 #CONTAINER=`docker ps | perl -wnale '/$ENV{image}/ and print $F[0]'`
