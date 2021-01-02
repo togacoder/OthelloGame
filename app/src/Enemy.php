6f887973c70530171bd1cd2b46d499f3bafc7751
togacoder <bike.dio@gmail.com>
2021-01-03 02:01:12 +0900

    関数整理

--- app/src/Enemy.php
+++ app/src/Enemy.php
@@ -2,15 +2,12 @@
 
 Class Enemy
 {
-    const DEBUG = true;
-
     public static function move($board, $playerColor) {
         $possiblePlaceArray = Piece::getPossiblePlaceArray($board, $playerColor);
         if (empty($possiblePlaceArray)) {
             echo "指せる手がありません。\n";
             return $board;
         }
-        //$position = self::chosePosition($possiblePlaceArray);
         $position = self::AI($board, $possiblePlaceArray, $playerColor);
         $board = Piece::setPiece($board, $position, $playerColor);
         echo "相手の手 : ([$position[0], $position[1]])\n";
@@ -18,54 +15,57 @@ Class Enemy
     }
 
     private static function AI($board, $possiblePlaceArray, $playerColor) {
-        $boardArray = array();
-        $positionArray = array();
-        foreach ($possiblePlaceArray as $possiblePlace) {
-            $position = array($possiblePlace[0], $possiblePlace[1]);
-            $getPointArray[] = self::getMaxDiffPoint($board, $possiblePlace, $playerColor);
-        }
+        $maxPointPlaceArray = self::getMaxPointPlaceArray($board, $possiblePlaceArray, $playerColor);
+        return self::getBestPosition($maxPointPlaceArray);
+    }
 
-        $keepPoint = array(0, 0);
-        foreach ($getPointArray as $index => $getPoint) {
-            if ($keepPoint[1] <= $getPoint) {
-                $keepPoint[0] = $index;
-                $keepPoint[1] = $getPoint;
+    private function getBestPosition($maxPointPlaceArray) {
+        $position = array();
+        $point = PHP_INT_MIN;
+        foreach ($maxPointPlaceArray as $maxPointPlace) {
+            if ($point <= $maxPointPlace[2]) {
+                $point = $maxPointPlace[2];
+                $position = array($maxPointPlace[0], $maxPointPlace[1]);
             }
         }
-
-        $position = array($possiblePlaceArray[$keepPoint[0]][0], $possiblePlaceArray[$keepPoint[0]][1]);
         return $position;
+    }
+
+    private function getMaxPointPlaceArray($board, $possiblePlaceArray, $color) {
+        $maxPointPlaceArray = array();
+        $position = array();
+        foreach ($possiblePlaceArray as $possiblePlace) {
+            $position = array($possiblePlace[0], $possiblePlace[1]);
+            $maxPointPlaceArray[] = array($position[0], $position[1], self::getMaxDiffPoint($board, $possiblePlace, $color));
+        }
+
+        return $maxPointPlaceArray;
     }
 
     private function getMaxDiffPoint($board, $possiblePlace, $playerColor) {
         $position = array($possiblePlace[0], $possiblePlace[1]);
         $point = $possiblePlace[2];
-
-        if (self::DEBUG) {
-            echo "着手可能手 [($position[0], $position[1])] : $point -> ";
-        }
-
         $board = Piece::setPiece($board, $position, $playerColor);
         $enemyPossiblePlaceArray = Piece::getPossiblePlaceArray($board, Player::getEnemyColor($playerColor));
-        if (empty($enemyPossiblePlaceArray)) {
+        $point = self::getMaxPoint($enemyPossiblePlaceArray);
+
+        return $point - $getMaxEnemyPoint;
+    }
+
+    private function getMaxPoint($possiblePlaceArray) {
+        $point = PHP_INT_MIN;
+        if (empty($possiblePlaceArray)) {
             return 0;
         }
 
-        $position = array();
-        $getMaxEnemyPoint = PHP_INT_MIN;
-        foreach ($enemyPossiblePlaceArray as $enemyPossiblePlace) {
-            if ($getMaxEnemyPoint < $enemyPossiblePlace[2]) {
-                $position = array($enemyPossiblePlace[0], $enemyPossiblePlace[1]);
-                $getMaxEnemyPoint = $enemyPossiblePlace[2];
+        $position = array(0, 0);
+        foreach ($possiblePlaceArray as $possiblePlace) {
+            if ($point <= $possiblePlace[2]) {
+                $point = $possiblePlace[2];
+                $position = array($possiblePlace[0], $possiblePlace[1]);
             }
         }
-
-        if (self::DEBUG) {
-            echo "相手の予想手 [($position[0], $position[1])] : $getMaxEnemyPoint\n";
-            Board::display($board);
-        }
-
-        return $point - $getMaxEnemyPoint;
+        return $point;
     }
 
     private static function chosePosition($possiblePlaceArray) {
@@ -79,4 +79,5 @@ Class Enemy
         }
         return $position;
     }
+
 }
